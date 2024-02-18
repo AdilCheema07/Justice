16e9717dac3b346e775af5f9a228f6601dc95331
AdilCheema07 <adcheema85@gmail.com>
2024-02-18 10:24:16 +0500

    Making Mobile Nav

---
+++ index.js
@@ -0,0 +1,10 @@
+function toggleMobileNavbar() {
+    var mobileNavbar = document.getElementById('mobile-navbar');
+    // Toggle the display property of the mobile navbar
+    if (mobileNavbar.style.display === 'none') {
+      mobileNavbar.style.display = 'block';
+    } else {
+      mobileNavbar.style.display = 'none';
+    }
+  }
+  
