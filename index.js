bfafc6ddb22bbc4828f0185ce4abbf0ea8df8149
AdilCheema07 <adcheema85@gmail.com>
2024-02-19 23:00:56 +0500

    Added Counter Animation

--- index.js
+++ index.js
@@ -8,3 +8,32 @@ function toggleMobileNavbar() {
     mobileNavbar.style.display = 'none';
   }
 }
+
+
+// Counter Animation 
+
+const counterUp = window.counterUp.default
+
+        const callback = entries => {
+            entries.forEach(entry => {
+                const el = entry.target
+                if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+                    counterUp(el, {
+                        duration: 1000,
+                        delay: 16,
+                    })
+                    el.classList.add('is-visible')
+                }
+            })
+        }
+
+        const IO = new IntersectionObserver(callback, { threshold: 1 })
+
+        let el = document.querySelector('.counter')
+        let al = document.querySelector('.counter1')
+        let sl = document.querySelector('.counter2')
+        let dl = document.querySelector('.counter3')
+        IO.observe(el)
+        IO.observe(al)
+        IO.observe(sl)
+        IO.observe(dl)
